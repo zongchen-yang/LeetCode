94ba02d8129c22ff2dc765d327e5ff3fd18298ad
Zongchen Yang <zongchenyang@Zongchens-MacBook-Pro.local>
2021-07-26 23:50:01 -0700

    LRU cache complete

--- LRUCache.js
+++ LRUCache.js
@@ -2,7 +2,8 @@
  * @param {number} capacity
  */
  var LRUCache = function(capacity) {
-
+  this.cache = new Map();
+  this.capacity = capacity;
 };
 
 /**
@@ -10,7 +11,13 @@
 * @return {number}
 */
 LRUCache.prototype.get = function(key) {
-
+ if (!this.cache.has(key)) {
+   return -1;
+ }
+ const temp = this.cache.get(key);
+ this.cache.delete(key);
+ this.cache.set(key, temp);
+ return temp;
 };
 
 /**
@@ -19,7 +26,13 @@ LRUCache.prototype.get = function(key) {
 * @return {void}
 */
 LRUCache.prototype.put = function(key, value) {
-
+  if (this.cache.has(key)) {
+    this.cache.delete(key);
+  }
+  this.cache.set(key, value);
+  if (this.cache.size > this.capacity) {
+    this.cache.delete(this.cache.keys().next().value);
+  }
 };
 
 /**
