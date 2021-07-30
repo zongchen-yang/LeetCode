2108870a052e4bbaf6ed4f3014b48743405e9360
Zongchen Yang <zongchenyang@Zongchens-MacBook-Pro.local>
2021-07-29 20:56:22 -0700

    container with most water

---
+++ containerWithMostWater.js
@@ -0,0 +1,18 @@
+/** 11
+ * @param {number[]} height
+ * @return {number}
+ */
+ var maxArea = function(height) {
+  let left = 0, right = height.length - 1;
+  let max = 0;
+  while (left < right) {
+      let shortest = Math.min(height[left], height[right]);
+      max = Math.max(max, shortest * (right - left));
+      if (height[left] <= height[right]) {
+          left ++;
+      } else {
+          right --;
+      }
+  }
+  return max;
+};
