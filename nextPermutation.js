7041b5270c6587d9e1bca9efc7e7c837a67c920f
Zongchen Yang <zongchenyang@Zongchens-MacBook-Pro.local>
2021-07-30 13:18:18 -0700

    next permutation

---
+++ nextPermutation.js
@@ -0,0 +1,32 @@
+/** 31
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var nextPermutation = function(nums) {
+  const reverse = (index) => {
+      let start = index;
+      let end = nums.length - 1;
+      while (start < end) {
+          [nums[start], nums[end]] = [nums[end], nums[start]];
+          start ++;
+          end --;
+      }
+  }
+
+  const nextLarge = (index) => {
+      for (let i = nums.length - 1; i > index; i--) {
+          if (nums[i] > nums[index]) {
+              return i;
+          }
+      }
+  }
+  for (let i = nums.length - 1; i >= 0; i--)  {
+      if (nums[i] > nums[i - 1]) {
+          [nums[nextLarge(i - 1)], nums[i - 1]] = [nums[i - 1], nums[nextLarge(i - 1)]];
+          reverse(i);
+          return;
+      }
+  }
+  nums.reverse();
+};
+
