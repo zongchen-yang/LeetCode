409e7e9485ca15deb67faa89207101c2a562e706
Zongchen Yang <zongchenyang@Zongchens-MacBook-Pro.local>
2021-07-30 18:45:24 -0700

    rotate image

---
+++ rotateImage.js
@@ -0,0 +1,20 @@
+/** 48
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+ var rotate = function(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+      for (let j = i; j < matrix[0].length; j++) {
+          [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+      }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+      let start = 0;
+      let end = matrix[0].length - 1;
+      while (start < end) {
+          [matrix[i][start], matrix[i][end]] = [matrix[i][end], matrix[i][start]];
+          start ++;
+          end --;
+      }
+  }
+};
