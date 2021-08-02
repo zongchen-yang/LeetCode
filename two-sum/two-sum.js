39221b0ea874f4c5c3a7a2dd1aef3cc475fc82e1
Zongchen Yang <zongchenisjack@gmail.com>
2021-08-02 10:17:54 -0700

    Time: 72 ms (94.36%), Space: 40.3 MB (44.50%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (map[diff] !== undefined) {
+            return [map[diff], i];
+        } else {
+            map[nums[i]] = i;
+        }
+    }
+};
