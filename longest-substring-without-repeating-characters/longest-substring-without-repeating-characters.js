8b678f2397029022b21fb19012c8dcc7e515f308
Zongchen Yang <zongchenisjack@gmail.com>
2021-09-07 23:25:18 -0700

    Time: 116 ms (62.74%), Space: 44.3 MB (59.45%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const map = {};
+    let start = 0;
+    let longest = 0;
+    let curr;
+    for (let i = 0; i < s.length; i++) {
+        curr = s[i];
+        if (map[curr] >= start) {
+            start = map[curr] + 1;
+        }
+        map[curr] = i;
+        if (i - start + 1 > longest) {
+            longest = i - start + 1;
+        }
+    }
+    return longest;
+};
