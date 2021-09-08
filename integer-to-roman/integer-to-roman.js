d7d9b8f80752fc099b5879565e4426dbcc640d2e
Zongchen Yang <zongchenisjack@gmail.com>
2021-09-07 23:37:42 -0700

    Time: 148 ms (57.14%), Space: 44.9 MB (68.97%) - LeetHub

---
+++ integer-to-roman/integer-to-roman.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    var map = {
+        M : 1000,
+        CM : 900,
+        D : 500,
+        CD : 400,
+        C : 100,
+        XC : 90,
+        L : 50,
+        XL : 40,
+        X : 10,
+        IX : 9,
+        V : 5,
+        IV : 4,
+        I : 1
+    }
+    var output = '';
+    while (num) {
+        for (var key in map) {
+            if (num >= map[key]) {
+                output += key.repeat(Math.floor(num / map[key]));
+                num = Math.floor(num % map[key]);
+            }
+        }
+    }
+    return output;
+};
