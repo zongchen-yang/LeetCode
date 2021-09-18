30908ea2e03a1ea79f0ce8b7c7279086e831b782
Zongchen Yang <zongchenisjack@gmail.com>
2021-09-18 11:28:58 -0700

    Time: 76 ms (81.98%), Space: 40.3 MB (66.84%) - LeetHub

--- decode-ways/decode-ways.js
+++ decode-ways/decode-ways.js
@@ -3,25 +3,21 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    if (s == null || s.length === 0) return 0;
-  if (s[0] === '0') return 0;
-
-  const dp = new Array(s.length + 1).fill(0);
-
-  dp[0] = 1;
-  dp[1] = 1;
-
-  for (let i = 2; i <= s.length; i++) {
-    const a = Number(s.slice(i - 1, i));  // last one digit
-    if (a >= 1 && a <= 9) {
-      dp[i] += dp[i - 1];
+    if (s[0] === '0' || s.length === 0) {
+        return 0;
     }
-
-    const b = Number(s.slice(i - 2, i));  // last two digits
-    if (b >= 10 && b <= 26) {
-      dp[i] += dp[i - 2];
+    const memo = new Array(s.length + 2).fill(0);
+    memo[0] = 1;
+    memo[1] = 1;
+    for (let i = 2; i <= s.length; i++) {
+        let a = Number(s[i-1]);
+        if (a > 0 && a < 10) {
+            memo[i] += memo[i-1];
+        }
+        let b = Number(s.slice(i-2, i));
+        if (b >= 10 && b <= 26) {
+            memo[i] += memo[i-2];
+        }
     }
-  }
-
-  return dp[s.length];
+    return memo[s.length];
 };
