9ff17ab8aa22c529e5f2d24c91f051e5db1b90db
Zongchen Yang <zongchenisjack@gmail.com>
2021-09-20 13:55:27 -0700

    Time: 186 ms (5.04%), Space: 43.5 MB (22.50%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.js
+++ validate-binary-search-tree/validate-binary-search-tree.js
@@ -10,9 +10,9 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root, min = null, max = null) {
+var isValidBST = function(root, left = null, right = null) {
     if (!root) return true;
-    if (min && root.val <= min.val) return false;
-    if (max && root.val >= max.val) return false;
-    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    if (left && left.val >= root.val) return false;
+    if (right && right.val <= root.val) return false;
+    return isValidBST(root.left, left, root) && isValidBST(root.right, root, right)
 };
