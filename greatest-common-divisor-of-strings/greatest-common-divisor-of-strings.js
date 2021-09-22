c470a0f06f3261ca21d5abf750feae8f485d2cc8
Zongchen Yang <zongchenisjack@gmail.com>
2021-09-21 22:39:43 -0700

    Time: 76 ms (68.35%), Space: 39.9 MB (63.29%) - LeetHub

---
+++ greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 !== str2 + str1) return '';
+    const gcd = (a, b) => {
+       return b === 0 ? a : gcd(b, a % b);
+    }
+    return str1.slice(0, gcd(str1.length, str2.length))
+};
