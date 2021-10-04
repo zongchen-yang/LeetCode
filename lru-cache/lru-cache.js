fecf727e804680a5454b12a42e9e35ec70eed188
Zongchen Yang <zongchenisjack@gmail.com>
2021-10-04 16:25:50 -0700

    Time: 736 ms (20.68%), Space: 121.2 MB (7.15%) - LeetHub

--- lru-cache/lru-cache.js
+++ lru-cache/lru-cache.js
@@ -1,43 +1,25 @@
-/**
- * @param {number} capacity
- */
-var LRUCache = function(capacity) {
-    this.map = new Map();
-    this.size = capacity;
-};
+class LRUCache {
+  constructor(capacity) {
+    this.cache = new Map();
+    this.capacity = capacity;
+  }
 
-/** 
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if (!this.map.has(key)) {
-        return -1;
+  get(key) {
+    if (!this.cache.has(key)) return -1;
+
+    const v = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, v);
+    return this.cache.get(key);
+  };
+
+  put(key, value) {
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
     }
-    let temp = this.map.get(key);
-    this.map.delete(key);
-    this.map.set(key, temp);
-    return temp;
-};
-
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function(key, value) {
-    if (this.map.has(key)) {
-        this.map.delete(key);
-    } 
-    this.map.set(key, value);
-    if (this.size < this.map.size) {
-        this.map.delete(this.map.keys().next().value);
+    this.cache.set(key, value);
+    if (this.cache.size > this.capacity) {
+      this.cache.delete(this.cache.keys().next().value);  // keys().next().value returns first item's key
     }
-};
-
-/** 
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
+  };
+}
