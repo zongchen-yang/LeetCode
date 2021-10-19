938bb39f6aefbb3487a2a185771728cd3b25040c
Zongchen Yang <zongchenisjack@gmail.com>
2021-10-18 19:34:28 -0700

    Time: 700 ms (29.18%), Space: 94.2 MB (52.60%) - LeetHub

--- lru-cache/lru-cache.js
+++ lru-cache/lru-cache.js
@@ -1,25 +1,20 @@
-class LRUCache {
-  constructor(capacity) {
+var LRUCache = function(capacity) {
     this.cache = new Map();
     this.capacity = capacity;
-  }
+};
 
-  get(key) {
+LRUCache.prototype.get = function(key) {
     if (!this.cache.has(key)) return -1;
-
-    const v = this.cache.get(key);
+    const temp = this.cache.get(key);
     this.cache.delete(key);
-    this.cache.set(key, v);
-    return this.cache.get(key);
-  };
+    this.cache.set(key, temp);
+    return temp;
+};
 
-  put(key, value) {
-    if (this.cache.has(key)) {
-      this.cache.delete(key);
-    }
+LRUCache.prototype.put = function(key, value) {
+    if (this.cache.has(key)) this.cache.delete(key);
     this.cache.set(key, value);
     if (this.cache.size > this.capacity) {
-      this.cache.delete(this.cache.keys().next().value);  // keys().next().value returns first item's key
+        this.cache.delete(this.cache.keys().next().value);
     }
-  };
-}
+};
