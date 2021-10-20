0771426aa1cfa8d15af6b2c3ec6089e3c65dfda1
Zongchen Yang <zongchenisjack@gmail.com>
2021-10-20 15:27:51 -0700

    Time: 154 ms (26.46%), Space: 49.2 MB (41.98%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.js
+++ subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -4,15 +4,15 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let map = new Map();
-    let sum = 0;
     let count = 0;
-    map.set(0,1);
-    for (let i=0;i<nums.length;i++) {
+    const hash = {};
+    hash[0] = 1;
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
-        if (map.has(sum-k)) count += map.get(sum-k);
-        if (map.has(sum)) map.set(sum, map.get(sum)+1);
-        else map.set(sum, 1);
+        if (hash[sum-k]) count += hash[sum-k];
+        if (hash[sum]) hash[sum] ++;
+        else hash[sum] = 1;
     }
     return count;
 };
