0cb99bb10a7ac251842695f9f5d97d79883292fd
Zongchen Yang <zongchenisjack@gmail.com>
2021-10-27 02:07:18 -0700

    Time: 148 ms (85.38%), Space: 43.5 MB (96.15%) - LeetHub

--- minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.js
+++ minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var connectSticks = function(sticks) {
-    if (sticks.length <= 1) return 0;
-    sticks.sort((a, b) => a - b);
-    const combined = [];
+    if (sticks.length < 2) return 0;
     let res = 0;
+    let combined = [];
+    sticks = sticks.sort((a, b) => a - b);
     while (sticks.length || combined.length > 1) {
-        let sum = 0;
+        let curr = 0;
         for (let i = 0; i < 2; i++) {
-            let condition = sticks.length && (!combined.length || sticks[0] < combined[0]);
-            sum += condition ? sticks.shift() : combined.shift();
+            let condition = sticks.length && (!combined.length || sticks[0] < combined[0] );
+            curr += condition ? sticks.shift() : combined.shift();
         }
-        combined.push(sum);
-        res += sum;
+        combined.push(curr);
+        res += curr;
     }
     return res;
 };
