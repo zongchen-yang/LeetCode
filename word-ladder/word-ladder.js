dfbda47e3f1a2fb92b726549218500430c4265e2
Zongchen Yang <zongchenisjack@gmail.com>
2021-11-09 15:39:13 -0800

    Time: 204 ms (78.62%), Space: 47.1 MB (55.53%) - LeetHub

---
+++ word-ladder/word-ladder.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    const wordSet = new Set(wordList)
+    let queue = [beginWord];
+    let steps = 1;
+    
+    while(queue.length) {
+        const next = [];
+        for(let word of queue) {
+            if(word === endWord) return steps;
+            for(let i = 0; i < word.length; i++) {
+                for(let j = 0; j < 26; j++) {
+                    const newWord = word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i+1);
+                    if(wordSet.has(newWord)) {
+                        next.push(newWord);
+                        wordSet.delete(newWord);
+                    }
+                }
+            }
+        }
+        queue = next
+        steps++;
+    }
+    return 0;    
+};
